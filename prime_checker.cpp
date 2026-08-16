c995322fc7f335717785d878b58cc54f3dedf76c
anugrahb14 <anugrahb2007@gmail.com>
2026-08-16 08:22:26 +0530

    Add prime number checker assignment

---
+++ prime_checker.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int number;
+    bool isPrime = true;
+    cout << "===== PRIME NUMBER CHECKER =====" << endl;
+    cout << "Enter a number: ";
+    cin >> number;
+    if (number <= 1)
+    {
+        isPrime = false;
+    }
+    else
+    {
+        for (int i = 2; i <= number / 2; i++)
+        {
+            if (number % i == 0)
+            {
+                isPrime = false;
+                break;
+            }
+        }
+    }
+    if (isPrime)
+    {
+        cout << number << " is a prime number." << endl;
+    }
+    else
+    {
+        cout << number << " is not a prime number." << endl;
+    }
+    return 0;
+}
